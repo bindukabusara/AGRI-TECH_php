54c3cfa86eaedcd6668c86ade9f0c6ff6ad456b3
bindukabusara <josuebindu97@gmail.com>
2023-06-20 18:21:10 +0300

    I changed the title

--- singup.php
+++ singup.php
@@ -131,28 +131,9 @@ if (isset($_POST['submit'])) {
     </section>
 
 
-    <!-- JavaScript -->
+
 
 </body>
 
 
 </html>
-<!-- Javascript -->
-
-<!--<script>
-        function redirectToPage(event) {
-            event.preventDefault(); // Prevent the default form submission
-            const nameInput = document.getElementById('name');
-            const name = nameInput.value;
-            const roleSelect = document.getElementById('role');
-            const selectedOption = roleSelect.value;
-            const contactInput = document.getElementById('contact');
-            const contact = contactInput.value;
-
-            if (selectedOption === 'farmer') {
-                window.location.href = `farmer.html?name=${name}`; // Redirect to farmer page with name parameter
-            } else if (selectedOption === 'whole seller') {
-                window.location.href = 'order.html'; // Redirect to whole seller page
-            }
-        }
-    </script>-->
