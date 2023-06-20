5d9f0cb36c1c81889d8416c928fb6839861b1022
bindukabusara <josuebindu97@gmail.com>
2023-06-20 17:51:43 +0300

    Inserted data into the database

--- order.php
+++ order.php
@@ -4,15 +4,14 @@ include 'connection.php'; // Include the database connection file
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form data
-    $name = $_POST['name']; // New line: Get the name from the form
-
+    $name = $_POST['name'];
     $product = $_POST['product'];
     $quantity = $_POST['quantity'];
     $totalCost = $_POST['totalCost'];
     $contact = $_POST['contact'];
 
     // Create the SQL INSERT query
-    $sql = "INSERT INTO order (name, product, quantity, totalCost, contact) VALUES ('$name', '$product', '$quantity', '$totalCost','$contact')"; // New line: Include the name in the query
+    $sql = "INSERT INTO `order` (name, product, quantity, totalCost, contact) VALUES ('$name', '$product', '$quantity', '$totalCost', '$contact')";
 
     if ($connection->query($sql) === TRUE) {
         echo "Data inserted successfully";
@@ -31,7 +30,7 @@ $connection->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> ORDER page </title>
+    <title>ORDER page</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="order.css">
@@ -39,7 +38,6 @@ $connection->close();
 
     <!-- Boxicons css-->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
 
     <style>
         /* Responsive styles */
@@ -82,10 +80,7 @@ $connection->close();
 <body>
 
     <nav class="navlist">
-
         <div class="sub">
-
-
             <ul>
                 <li>
                     <h1>Agri<span>Trans</span></h1>
@@ -95,34 +90,25 @@ $connection->close();
                 <li><a href="">About Us</a></li>
             </ul>
         </div>
-
     </nav>
-
 
     <section class="container forms">
         <h1>Agri<span>Trans</span></h1>
         <div class="form login">
             <div class="form-content">
-
                 <h2>Order</h2><br>
                 <p id="greetingMessage"></p><br>
-
-                <form id="requestForm" action="info2.html" onsubmit="constructURL(event)">
-
-
+                <form id="requestForm">
                     <div class="field input-field">
                         <label for="product">Product<span class="required"></span></label>
                         <select class="selectProduct" id="product" name="product" required>
                             <option value="">-- Please select a product --</option>
-
                             <option value="beans">Beans</option>
                             <option value="rice">Rice</option>
                             <option value="wheat">Wheat</option>
                             <option value="maize">Maize</option>
                             <option value="peas">Peas</option>
-
                         </select>
-
                     </div>
                     <div class="field input-field">
                         <label for="quantity">Quantity (KG)<span class="required"></span></label>
@@ -132,26 +118,20 @@ $connection->close();
                         <label>Total Cost (SHS)</label>
                         <input type="number" placeholder="" class="input" id="totalCost" name="totalCost" readonly>
                     </div><br><br><br>
-
-
-
                     <div class="field button-field">
-                        <button><strong>Complete Order</strong></button>
+                        <button type="submit"><strong>Complete Order</strong></button>
                     </div><br>
-
-
                 </form>
             </div>
         </div>
     </section>
-
-    <!-- Javascript -->
 
     <!-- JavaScript -->
     <script>
         // Get the URL parameters
         const urlParams = new URLSearchParams(window.location.search);
         const name = urlParams.get('name');
+        const contact = urlParams.get('contact');
 
         // Get references to the elements
         const greetingMessage = document.getElementById('greetingMessage');
@@ -160,7 +140,6 @@ $connection->close();
         const totalCostInput = document.getElementById('totalCost');
 
         // Update the greeting message with the name
-        const welcomeMessage = document.getElementById('greetingMessage');
         greetingMessage.textContent = `Hey ${name}, do you want to order grains (beans, Rice, Wheat, etc.) and get them delivered to your market?`;
 
         // Add an event listener to detect changes in the inputs
@@ -193,10 +172,36 @@ $connection->close();
 
             totalCostInput.value = isNaN(totalCost) ? '' : totalCost.toFixed(2);
         }
+
+        // Add an event listener to the form's submit event
+        const requestForm = document.getElementById('requestForm');
+        requestForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Gather the form data
+            const formData = new FormData(requestForm);
+
+            // Create and configure the XMLHttpRequest object
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', '<?php echo $_SERVER['PHP_SELF']; ?>', true);
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+            // Define the callback function when the request completes
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    console.log(xhr.responseText); // Log the response from the server
+                    // Redirect to the Info Page with query parameters
+                    const redirectURL = `info2.html?name=${name}&product=${productSelect.value}&quantity=${quantityInput.value}&totalCost=${totalCostInput.value}`;
+                    window.location.href = redirectURL;
+                } else {
+                    console.error('Error inserting data:', xhr.status);
+                }
+            };
+
+            // Send the AJAX request
+            xhr.send(formData);
+        });
     </script>
-
-
-
 </body>
 
 </html>
