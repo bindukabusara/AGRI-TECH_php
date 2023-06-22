d1b5b6a53199b210b25a04055568009210df6f62
bindukabusara <josuebindu97@gmail.com>
2023-06-22 16:31:18 +0300

    I added a logo

--- log.php
+++ log.php
@@ -103,7 +103,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="sub">
             <ul>
                 <li>
-                    <h1>Agri<span>Trans</span></h1>
+                    <img src="logoa.png" alt="logo" width="100px" length="100px">
                 </li>
                 <li><a href="home.php"><b>Home</b></a></li>
                 <li><a href="">Service</a></li>
