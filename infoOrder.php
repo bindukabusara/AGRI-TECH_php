dc734dd05b4a0efe7162cbdd0cc624085d12b6d9
bindukabusara <josuebindu97@gmail.com>
2023-07-05 09:35:59 +0300

    update

--- infoOrder.php
+++ infoOrder.php
@@ -92,24 +92,39 @@
             background-color: #e6e6e6;
         }
     </style>
-    </style>
+
+
+    <script>
+        function logout() {
+            if (confirm('Are you sure you want to log out?')) {
+                // User clicked "Yes," redirect to the home page
+                window.location.href = 'home.php';
+            } else {
+                // User clicked "No," do nothing, stay on the same page
+            }
+        }
+    </script>
+
 
 </head>
 
 <body>
+
     <nav class="navlist">
         <div class="sub">
             <ul>
                 <li>
                     <img src="logoa.png" alt="logo" width="100px" length="100px">
                 </li>
-                <li><a href="home.php"><b>Home</b></a></li>
+                <li><a href="farmer.php">Farmer</a></li>
                 <li><a href="">Service</a></li>
                 <li><a href="">About Us</a></li>
+                <li><a href="history.php">History</a></li>
                 <li><a href="admin.html">Admin</a></li>
+                <li><a href=" javascript:void(0);" onclick="logout()">Log out</a></li>
             </ul>
         </div>
-    </nav><BR><br><br><br><br>
+    </nav><BR><br><br><br><br><br><br>
     <center>
         <h2>WHOLE SELLER DATABASE TRACKING (admin view)</h2>
     </center>
